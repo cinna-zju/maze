a899bcf8d201c1538a3382200d2ac6b5de4523da
cinna-zju <569095288@qq.com>
2018-10-10 16:18:29 +0200

    Test Control

--- src/Controller/ButtonCtlr.java
+++ src/Controller/ButtonCtlr.java
@@ -1,9 +1,25 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 
 public class ButtonCtlr {
+
+    @FXML
+    private Button Monster;
+
+    @FXML
+    private void onClick(MouseEvent e){
+        System.out.println(e.getSource());
+        if(e.getSource() == Monster){
+            Monster.setDisable(true);
+        }
+
+    }
 
 
 }
