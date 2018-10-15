ab6bfa579b7c0e956ef30aef958d75b2ab3f9091
cinna-zju <569095288@qq.com>
2018-10-15 16:05:50 +0200

    Ctrl mazeCreation

--- Character.java
+++ Character.java
@@ -1,4 +1,5 @@
-package sample;
+
+		package sample;
 
 import java.io.File;
 
@@ -19,8 +20,11 @@ public class Character extends HBox{
 	public String choice;
 	public int cl1,cl2,cl3,cl4;
 	public VBox v1, v2, v3, v4;
+	private Layout layout;
+	private ModelChara model;
 	
 	public Character() {
+		layout = new Layout(); //to have access to button "map";
 		this.setPadding(new Insets(10,10,10,10));
 		this.setSpacing(10);
 		this.setPrefSize(50,50);
@@ -28,11 +32,71 @@ public class Character extends HBox{
 		cl2 = 0;
 		cl3 = 0;
 		cl4 = 0;
-		init();
-		
+		controller();		
 	}
 	
-	public void init() {
+	public void layoutProf() {
+		//Professor
+		Character(100,200,50);
+		Label life1 = new Label("Life : " + model.getLife());
+		Label Skill1 = new Label("Skill : jump");
+		
+		Label power1 = new Label("Power : " +model.getPower());
+		
+		Label defense1 = new Label("Defense : " +model.getDefense());
+
+		if(cl1== 1) {
+			v1.getChildren().addAll(life1,Skill1,power1,defense1);
+		}		
+	}
+	
+	private void Character(int life, int power, int defense) {
+		// TODO Auto-generated method stub
+		model = new ModelChara(life,power,defense);
+	}
+
+	public void layoutWarrior() {
+		Character(180,100,300);
+		Label life2 = new Label("Life : " +model.getLife());
+		Label Skill2 = new Label("Skill : climb");
+		
+		Label power2 = new Label("Power : " +model.getPower());
+		
+		Label defense2 = new Label("Defense : " +model.getDefense());
+		if(cl2 == 1) {
+			v2.getChildren().addAll(life2,Skill2,power2,defense2);
+		}
+	}
+	
+	public void layoutKing() {
+		//Characteristics
+		Character(150,80,50);
+		Label life3 = new Label("Life : " + model.getLife());
+		Label Skill3 = new Label("Skill : sword");
+		
+		Label power3 = new Label("Power : " + model.getPower());
+		
+		Label defense3 = new Label("Defense : " + model.getDefense());
+		if(cl3 == 1) { 
+		v3.getChildren().addAll(life3,Skill3,power3,defense3);
+		}
+	}
+
+	public void layoutSoldier() {	
+		Character(200,250,150);
+		Label life4 = new Label("Life : " + model.getLife());
+		Label Skill4 = new Label("Skill : jump");
+
+		Label power4 = new Label("Power : " +model.getPower());
+
+		Label defense4 = new Label("Defense : "+model.getDefense());
+		if(cl4 == 1) {
+		v4.getChildren().addAll(life4,Skill4,power4,defense4);
+		}
+	}
+	
+	public void controller() {
+		skills();
 		// HBox for 4 characters
 		choice = "";
 	//Professor
@@ -41,30 +105,27 @@ public class Character extends HBox{
 		v1.setSpacing(10);
 		v1.setPadding(new Insets(10,10,10,10));
 		v1.setStyle("-fx-background-color: #eff144"); //yellow
+		
+		Label name1 = new Label("Professor");
+		
+		// image...
+				Image prof = new Image("/professor.png",30,30,false,false);
+				ImageView iv1 = new ImageView();
+
+				iv1.setImage(prof);
+				v1.getChildren().addAll(name1,iv1);
+		
 		v1.setOnMouseClicked(event->{
 			//Characteristics
-				choice = "prof";
-			Label life1 = new Label("Life : 100");
-			Label Skill1 = new Label("Skill : jump");
-			Label power1 = new Label("Power : 200");
-			Label defense1 = new Label("Defense : 50");
-			if(cl1== 0) {
-			v1.getChildren().addAll(life1,Skill1,power1,defense1);
-			}
+			choice = "prof";
+	
 			v1.setStyle("-fx-background-color: #8df144"); // green
 			v2.setStyle("-fx-background-color: #eff144"); //yellow
 			v3.setStyle("-fx-background-color: #eff144"); //yellow
 			v4.setStyle("-fx-background-color: #eff144"); //yellow
-			cl1 = 1;
+			cl1 ++;
+			layoutProf();
 			});
-		
-		Label name1 = new Label("Professor");
-		// image...
-		Image prof = new Image("/professor.png",30,30,false,false);
-		ImageView iv1 = new ImageView();
-
-		iv1.setImage(prof);
-		v1.getChildren().addAll(name1,iv1);
 
 	//Warrior	
 		cl2 = 0;
@@ -74,19 +135,13 @@ public class Character extends HBox{
 		v2.setStyle("-fx-background-color: #eff144"); 
 		v2.setOnMouseClicked(event->{
 			//Characteristics
-				choice = "warrior";
-					Label life2 = new Label("Life : 180");
-					Label Skill2 = new Label("Skill : climb");
-					Label power2 = new Label("Power : 100");
-					Label defense2 = new Label("Defense : 300");
-					if(cl2 == 0) {
-						v2.getChildren().addAll(life2,Skill2,power2,defense2);
-					}
+				choice = "warrior";					
 					v2.setStyle("-fx-background-color: #8df144"); //green
 					v1.setStyle("-fx-background-color: #eff144"); //yellow
 					v3.setStyle("-fx-background-color: #eff144"); //yellow
 					v4.setStyle("-fx-background-color: #eff144"); //yellow
-					cl2 = 1;
+					cl2 ++;
+					layoutWarrior();
 					});
 			
 		Label name2 = new Label("Warrior");
@@ -104,19 +159,13 @@ public class Character extends HBox{
 		v3.setStyle("-fx-background-color: #eff144"); 
 		v3.setOnMouseClicked(event->{
 			choice = "king";
-		//Characteristics
-				Label life3 = new Label("Life : 150");
-				Label Skill3 = new Label("Skill : sword");
-				Label power3 = new Label("Power : 80");
-				Label defense3 = new Label("Defense : 50");
-				if(cl3 == 0) { 
-				v3.getChildren().addAll(life3,Skill3,power3,defense3);
-				}
+		
 				v3.setStyle("-fx-background-color: #8df144"); // green
 				v1.setStyle("-fx-background-color: #eff144"); //yellow
 				v2.setStyle("-fx-background-color: #eff144"); //yellow
 				v4.setStyle("-fx-background-color: #eff144"); //yellow
-				cl3 = 1;
+				cl3 ++;
+				layoutKing();
 				});
 		Label name3 = new Label("King");
 		// image...
@@ -135,18 +184,12 @@ public class Character extends HBox{
 		//Characteristics
 			choice = "soldier";
 			
-				Label life4 = new Label("Life : 200");
-				Label Skill4 = new Label("Skill : jump");
-				Label power4 = new Label("Power : 250");
-				Label defense4 = new Label("Defense : 150");
-				if(cl4 == 0) {
-				v4.getChildren().addAll(life4,Skill4,power4,defense4);
-				}
 				v4.setStyle("-fx-background-color: #8df144"); 
 				v1.setStyle("-fx-background-color: #eff144"); //yellow
 				v2.setStyle("-fx-background-color: #eff144"); //yellow
 				v3.setStyle("-fx-background-color: #eff144"); //yellow
-				cl4 = 1;
+				cl4 ++;
+				layoutSoldier();
 				});
 		Label name4 = new Label("Soldier");
 		// image...
@@ -158,3 +201,4 @@ public class Character extends HBox{
 		this.getChildren().addAll(v1,v2,v3,v4);
 		}
 }
+	
